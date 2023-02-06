19f105452e3be34c2f1eece995f877879be8d993
Tayan21 <bostybaev@gmail.com>
2023-02-06 13:01:04 +0600

    fixed drums

--- lecture21/main.js
+++ lecture21/main.js
@@ -11,3 +11,18 @@ document.addEventListener('keydown', function(e) {
   audio.currentTime = 0;
   audio.play()
 })
+
+document.addEventListener('click', function(e) {
+  let key = e.target.getAttribute('data-key')
+  console.log(key)
+  let drum = document.querySelector(`[data-key=${key}]`)
+  let audio = document.querySelector(`[data-a=${key}]`)
+
+  drum.classList.add('hit')
+  setTimeout(() => {
+    drum.classList.remove('hit')
+  }, 100)
+
+  audio.currentTime = 0;
+  audio.play()
+})

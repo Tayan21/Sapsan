5311bd273c79b5e24fde0f429582e05c7ba700ef
Tayan21 <bostybaev@gmail.com>
2023-02-07 18:01:13 +0600

    add some changes at lecture21

--- lecture21/main.js
+++ lecture21/main.js
@@ -14,7 +14,6 @@ document.addEventListener('keydown', function(e) {
 
 document.addEventListener('click', function(e) {
   let key = e.target.getAttribute('data-key')
-  console.log(key)
   let drum = document.querySelector(`[data-key=${key}]`)
   let audio = document.querySelector(`[data-a=${key}]`)
 
@@ -26,3 +25,40 @@ document.addEventListener('click', function(e) {
   audio.currentTime = 0;
   audio.play()
 })
+
+const asyncFunc = async (url) => {
+  const res = await fetch(url)
+  const json = await res.json()
+  return json 
+} 
+
+
+asyncFunc("https://my-json-server.typicode.com/Tayan21/projectserver/drums")
+    .then(data => {
+      for(let i = 0; i < data.length; i++) {
+        if(data[i]['name'] == "drum") {
+          let drums = document.createElement('div')
+          drums.setAttribute('class', data[i]['name'])
+          drums.style.background = `url(${data[i]['image']})`
+          document.body.appendChild(drums)
+          for(let i = 0; i < data.length-1; i++) {
+            let div = document.createElement('div')
+            div.setAttribute('data-key', data[i]['data-key'])
+            div.setAttribute('class', data[i].name) 
+            drums.append(div)
+          }
+        }
+      }
+      
+
+      for(let i = 0; i < data.length-1; i++) {
+        let audio = document.createElement('audio')
+        audio.setAttribute('data-a', data[i]['data-a'])
+        audio.setAttribute('src', data[i]['folder']) 
+        document.body.appendChild(audio)
+      }
+
+    })
+    .catch(e => console.log(e.message))
+
+

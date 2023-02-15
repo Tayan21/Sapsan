d5139407a6c97f6b19ad759334b5c0bc79a32f31
Tayan21 <bostybaev@gmail.com>
2023-02-15 16:08:18 +0600

    add lecture24

--- lecture21/main.js
+++ lecture21/main.js
@@ -1,67 +1,65 @@
-document.addEventListener('keydown', function(e) {
-  let key = e.key
-  let drum = document.querySelector(`[data-key=${key}]`)
-  let audio = document.querySelector(`[data-a=${key}]`)
+document.addEventListener("keydown", function (e) {
+  let key = e.key;
+  let drum = document.querySelector(`[data-key=${key}]`);
+  let audio = document.querySelector(`[data-a=${key}]`);
 
-  drum.classList.add('hit')
+  drum.classList.add("hit");
   setTimeout(() => {
-    drum.classList.remove('hit')
-  }, 100)
+    drum.classList.remove("hit");
+  }, 100);
 
   audio.currentTime = 0;
-  audio.play()
-})
+  audio.play();
+});
 
-document.addEventListener('click', function(e) {
-  let key = e.target.getAttribute('data-key')
-  let drum = document.querySelector(`[data-key=${key}]`)
-  let audio = document.querySelector(`[data-a=${key}]`)
+document.addEventListener("click", function (e) {
+  let key = e.target.getAttribute("data-key");
+  let drum = document.querySelector(`[data-key=${key}]`);
+  let audio = document.querySelector(`[data-a=${key}]`);
 
-  drum.classList.add('hit')
+  drum.classList.add("hit");
   setTimeout(() => {
-    drum.classList.remove('hit')
-  }, 100)
+    drum.classList.remove("hit");
+  }, 100);
 
   audio.currentTime = 0;
-  audio.play()
-})
+  audio.play();
+});
 
 const asyncFunc = async (url) => {
-  const res = await fetch(url)
-  const json = await res.json()
-  return json 
-} 
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
+};
 
 asyncFunc("https://my-json-server.typicode.com/Tayan21/projectserv/drums")
-    .then(data => {
-      for(let i = 0; i < data.length; i++) {
-        if(data[i]['name'] == "drum") {
-          let drums = document.createElement('div')
-          drums.setAttribute('class', data[i]['name'])
-          drums.style.background = `url(${data[i]['image']})`
-          document.body.appendChild(drums)
-          for(let i = 0; i < data.length-1; i++) {
-            let div = document.createElement('div')
-            let key = document.createElement('div')
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i]["name"] == "drum") {
+        let drums = document.createElement("div");
+        drums.setAttribute("class", data[i]["name"]);
+        drums.style.background = `url(${data[i]["image"]})`;
+        document.body.appendChild(drums);
+        for (let i = 0; i < data.length - 1; i++) {
+          let div = document.createElement("div");
+          let key = document.createElement("div");
 
-            key.innerHTML = `${data[i]['data-key'].toUpperCase()}`
-            div.append(key)
+          key.innerHTML = `${data[i]["data-key"].toUpperCase()}`;
+          div.append(key);
 
-            div.setAttribute('data-key', data[i]['data-key'])
-            div.setAttribute('class', data[i].name)
-            div.style.background = `url(${data[i]['image']})` 
-            drums.append(div)
-          }
+          div.setAttribute("data-key", data[i]["data-key"]);
+          div.setAttribute("class", data[i].name);
+          div.style.background = `url(${data[i]["image"]})`;
+          drums.append(div);
         }
       }
-      
-      for(let i = 0; i < data.length-1; i++) {
-        let audio = document.createElement('audio')
-        audio.setAttribute('data-a', data[i]['data-a'])
-        audio.setAttribute('src', data[i]['folder']) 
-        document.body.appendChild(audio)
-      }
-    })
-    .catch(e => console.log(e.message))
+    }
 
-
+    for (let i = 0; i < data.length - 1; i++) {
+      let audio = document.createElement("audio");
+      audio.setAttribute("data-a", data[i]["data-a"]);
+      audio.setAttribute("src", data[i]["folder"]);
+      document.body.appendChild(audio);
+    }
+  })
+  .catch((e) => console.log(e.message));
---
+++ lecture24/app.js
@@ -0,0 +1,18 @@
+const button = document.getElementById('button')
+const img = document.getElementById('photo')
+
+
+const asyncFunc = async (url) => {
+    const res = await fetch(url)
+    const json = await res.json()
+    return json 
+  } 
+  
+button.addEventListener('click', () => {
+    try {
+        asyncFunc("https://my-json-server.typicode.com/Tayan21/projectserv/drums")
+        .then(data => img.src=`${data[data.length-1].image}`)
+    } catch(er) {
+        console.log(er.message)
+    }
+})

ccf9a7b7acd96a9b4688aef13195e5b726e35d73
Tayan21 <bostybaev@gmail.com>
2023-02-28 16:10:15 +0600

    add lecture 25 and 27

--- lecture24/app.js
+++ lecture24/app.js
@@ -6,13 +6,16 @@ const asyncFunc = async (url) => {
     const res = await fetch(url)
     const json = await res.json()
     return json 
-  } 
-  
+} 
+ 
 button.addEventListener('click', () => {
     try {
-        asyncFunc("https://my-json-server.typicode.com/Tayan21/projectserv/drums")
+        asyncFunc("https://my-json-server.typicode.com/Tayan21/projectserv/drms")
         .then(data => img.src=`${data[data.length-1].image}`)
+        
     } catch(er) {
-        console.log(er.message)
+        console.error(er)
     }
 })
+
+
---
+++ lecture25/src/components/Greeting.tsx
@@ -0,0 +1,9 @@
+export function Greeting() {
+    return(
+        <h1>Hello World!</h1>
+        <h1></h1>
+        <h2></h2>
+        <h3></h3>
+        <h4></h4>
+    )
+}

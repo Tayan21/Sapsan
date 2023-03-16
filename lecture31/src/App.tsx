fec83897dd2728b3484038a203365e9900ef25b3
Tayan21 <bostybaev@gmail.com>
2023-03-16 20:29:25 +0600

    add lecture31

---
+++ lecture31/src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { ITodo } from "./types/data";
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value)
+  }
+  const keyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if(e.key === "Enter") {
+        addTodo()
+    }
+  }
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+      setValue('')
+    }
+  };
+
+  const removeTodo = (id: number): void => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  } 
+
+  const toggleTodo = (id: number): void => {
+    setTodos(todos.map(todo => {
+        if(todo.id !== id) {
+            return todo;
+        } else {
+             return {
+              ...todo,
+              complete: !todo.complete  
+             }
+        }
+    }))
+  } 
+
+  return (
+    <div>
+      <div>
+        <input value={value} onChange={handleChange} onKeyDown={keyDown} />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo}/>
+    </div>
+  );
+};
+
+export default App;
